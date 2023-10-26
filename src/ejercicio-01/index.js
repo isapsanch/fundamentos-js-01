6880c73a260ab89fb5a9f2e983f39ffbc28389b2
isapsanch <isabelpsanch@hotmail.com>
2023-10-26 19:29:38 +0200

    Añadidio el Ejercicio 1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,29 @@
+// Ejercicio 1: Variables y tipos de datos.
+//1. Variable "nombre"
+const nombre = "Juan"
+
+//2. Variable edad
+const edad = 55
+
+//3. Variable esEstudante
+const esEstudante = true
+
+//4. Variable mensaje
+const mensaje = 'Hola, mi nombre es '+ nombre +' y tengo '+ edad+'años. Soy estudiante: '+ esEstudante +'.'
+
+//5-6. Crea variable `num1´y asigale otro numero y un `num2``
+const num1 = 10
+const num2 = 5
+
+//7. Realiza operaciones aritméticas.
+
+const suma = num1+num2
+const resta = num1-num2
+const multiplicacion = num1*num2
+const division = num1/num2
+
+console.log(mensaje)
+console.log('Suma: ', suma)
+console.log('Resta: ', resta)
+console.log('Multiplicacion: ', multiplicacion)
+console.log('Division: ', division)
