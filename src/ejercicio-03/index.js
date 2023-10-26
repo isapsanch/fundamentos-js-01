3c956a4bb8aaa62771ceae0ea485c475280012a7
isapsanch <isabelpsanch@hotmail.com>
2023-10-26 19:57:39 +0200

    Añadir Ejercicio 3

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,15 @@
+// Ejercicio 3: Operador Ternario y Tipos de Datos
+
+//1. Variable número
+const numero = 15
+
+//2. Operadorr ternario para asignar tipoNumero
+const tipoNumero = numero > 0 ? 'positivo' : 'negativo o cero'
+console.log('El numero es '+ tipoNumero)
+
+//3. Variable cadena
+const cadena = 'Hola Mundo'
+
+//4. Operador ternario para asignar longitudCadena
+const longitudCadena = cadena.length > 5 ? 'larga' : 'corta'
+console.log('La cadena es '+ longitudCadena)
